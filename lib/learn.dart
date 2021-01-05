d4de7a501d944ee0662c5723e086dc9bda2eeb7b
lakshmi durga <lakshmigutti08@gmail.com>
2021-01-05 17:59:00 +0530

    error remove

---
+++ lib/learn.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class mylearnings extends StatefulWidget {
+  @override
+  _mylearningsState createState() => _mylearningsState();
+}
+
+class _mylearningsState extends State<mylearnings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,54 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => ;
+  _MyAppState createState() => _MyAppState();
 }
-class MyAppState extends State<MyApp> {
-  var questionIndex=0;
-  void answerQuestion(){
 
-  }
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {}
 
   @override
   Widget build(BuildContext context) {
-    var questions=[
+    var questions = [
       'what is your favorite colour',
       'what is your favourite animal'
     ];
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: Scaffold(
-           appBar: AppBar(
-             title: Text("Android studio"),
-           ),
-           body: Column(
-             children: [
-           Text(questions[0]),
-               RaisedButton(child: Text('BLUE'),onPressed: ()=> print("first option chosen")),
-               RaisedButton(child: Text('WHITE'),onPressed: ()=> print("second option chosen")),
-               RaisedButton(child: Text('PINK'),onPressed: ()=> print("third option chosen")),
-               Text(questions[1]),
-               RaisedButton(child: Text('CAT') ,onPressed: ()=>print("first option chosen")),
-               RaisedButton(child: Text('DOG') , onPressed: ()=>print('second option chosen')),
-               RaisedButton(child: Text('RABBIT') , onPressed: ()=>print('third option chosen'))
-             ],
-
-           ),
-            ),
-        );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Android studio"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[0]),
+            RaisedButton(
+                child: Text('BLUE'),
+                onPressed: () => print("first option chosen")),
+            RaisedButton(
+                child: Text('WHITE'),
+                onPressed: () => print("second option chosen")),
+            RaisedButton(
+                child: Text('PINK'),
+                onPressed: () => print("third option chosen")),
+            Text(questions[1]),
+            RaisedButton(
+                child: Text('CAT'),
+                onPressed: () => print("first option chosen")),
+            RaisedButton(
+                child: Text('DOG'),
+                onPressed: () => print('second option chosen')),
+            RaisedButton(
+                child: Text('RABBIT'),
+                onPressed: () => print('third option chosen'))
+          ],
+        ),
+      ),
+    );
   }
 }
